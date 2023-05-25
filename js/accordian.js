7418c71cccf6b0a38c6d3ddb99a3172c0863ea0b
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2023-05-25 21:24:59 +0200

    skelet

---
+++ js/accordian.js
@@ -0,0 +1,22 @@
+let elements = document.getElementsByClassName('recruitere-accordian')
+
+
+addAcordian(elements);
+
+
+
+
+function addAcordian(coll) {
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
