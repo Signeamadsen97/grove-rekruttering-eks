df8c3a0f8de21fade2ac336687b26c28a1bcd6ba
anna202c <fie904@gmail.com>
2023-05-29 21:40:37 +0200

    done

--- js/carousel.js
+++ js/carousel.js
@@ -1,42 +1,47 @@
 startKarrusellen();
-//laver variablen i og giver den værdien 0, laver varibalen billeder som er et array med billeder,
-//skaber en varibel kaldet tid, som tildeles en værdi senere.
 var i = 0;
-var billeder = ["img/ny-karusel-1.svg", "img/ny-karusel-2.svg", "img/ny-karusel-3.svg"];
+var billeder1 = ["img/ny-karusel-1.svg", "img/ny-karusel-2.svg", "img/ny-karusel-3.svg"];
+var billeder2 = ["img/ny-karusel-2.svg", "img/ny-karusel-3.svg", "img/ny-karusel-1.svg"];
+var billeder3 = ["img/ny-karusel-3.svg", "img/ny-karusel-1.svg", "img/ny-karusel-2.svg"];
+
 var tid;
 
-//laver en funktion som skal få karrusellen til at køre med et defineret interval under. 
-//Den skifter billede hvert 5 sekund, og men er ikke kaldt endnu
 function startKarrusellen() {
     tid = setInterval(nextImage, 5000);
 }
-//Denne funktion stopper karrusellens timer når den kaldes
 function stopKarrusellen() {
     clearInterval(tid);
 }
-//denne funktion regner en i værdi ved at tage nuværende værdi + 1. 
-//Derefter ses der hvor mange gange den går op i arrayet "billeder" længde,
-//og hvis der er en resterende, er det den nye værdi. Samtidig kalder den funktioneR
-//som  opdaterer billedet der vises og den styling der er på knapperne
 function nextImage() {
-    i = (i + 1) % billeder.length;
+    i = (i + 1) % billeder1.length;
     updateImage();
     updateButtonStyles();
 }
 
-//her bliver funktionen  updataimage skabt, som hele tiden opdaterer hvilken billede som vises
-//her bruger den før declared value som "i" har fået til at beslutte hvilket billede
-//i arrayet som skal vises
-function updateImage() {
-    document.getElementById("imageGallery").src = billeder[i];
+function nextImage() {
+    i = (i + 1) % billeder2.length;
+    updateImage();
+    updateButtonStyles();
 }
-//her defineres variablerne et, to og tre, som trækkes fra knapperne i HTML
+
+function nextImage() {
+    i = (i + 1) % billeder3.length;
+    updateImage();
+    updateButtonStyles();
+}
+
+function updateImage() {
+    document.getElementById("imageGallery1").src = billeder1[i];
+    document.getElementById("imageGallery2").src = billeder2[i];
+    document.getElementById("imageGallery3").src = billeder3[i];
+}
+
+
+
 let et = document.querySelector('.et');
 let to = document.querySelector('.to');
 let tre = document.querySelector('.tre');
 
-//her bliver funktionen skabt som opdaterer button styles ud fra hvilken værdi "i" har
-//er lavet med en if else if statement
 function updateButtonStyles() {
     if (i == 0){
         et.style.backgroundColor = 'rgb(165, 165, 165)';
@@ -55,10 +60,6 @@ function updateButtonStyles() {
 
           
 }
-//her er funktionen skiftBillede lavet, som også ligger på alle knapperne i HTML
-//klikkes der på knappen, bliver funktionen som stopper karrusellen kaldt, og her efter tjekkes
-//der værdien af knappen som er trykket på, ud fra denne tildeles "i" en ny værdi.
-//efter billederne er skiftet, opdateres stylesne og karrusellen startes igen
 function skiftBillede(klikket) {
     stopKarrusellen();
 if (klikket === 1){
