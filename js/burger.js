ca79951d98f4755b3696d40053a4426006d397ce
kamiberta <119923497+kamiberta@users.noreply.github.com>
2023-05-30 11:45:30 +0200

    fixed array/loop

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,10 @@
 const navbar = document.querySelector('.navbar');
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelectorAll('.nav-links li');
+const hamburger = document.querySelector('.hamburger')
+const navLinks = [...document.querySelectorAll('.nav-links li')];
 
-for (i = 0; i < navLinks.length; i++) {
-    content.style.display = block;
-    }
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].style.display = "block";
+}
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('nav-open');
