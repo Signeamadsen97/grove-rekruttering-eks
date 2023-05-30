8b76cff164316d4fd7947139bb6709746af8259c
kamiberta <119923497+kamiberta@users.noreply.github.com>
2023-05-30 10:21:08 +0200

    .

--- js/burger.js
+++ js/burger.js
@@ -2,6 +2,10 @@ const navbar = document.querySelector('.navbar');
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelectorAll('.nav-links li');
 
+for (i = 0; i < navLinks.length; i++) {
+    content.style.display = block;
+    }
+
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('nav-open');
     hamburger.classList.toggle('hamburger-open');
